29e6bdfe7a7093029c73a112ba50d5cfbd9f8711
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-10-19 08:37:37 +0800

    asdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import React, { createContext, useEffect, useState } from "react" 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from "./Home";
-import NavBar from "./NavBar";
-import HeatMap from "./HeatMap";
+import React from 'react'
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/heatmap" element={<HeatMap />} />
-      </Routes>
-    </BrowserRouter>
-    </>
+    <div>App</div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </>,
+)
